df7e1633f481c90bb5bf5823b9e4cc0cc1c607d6
shirs12 <93188009+shirs12@users.noreply.github.com>
2022-02-15 13:57:26 +0200

    Delete my-app directory

--- my-app/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import AppNavigator from "./navigation/AppNavigation";
-import { StyleSheet, Text, View, Button } from "react-native";
-
-export default function App() {
-  return (
-    <AppNavigator/>
-  );
-}
